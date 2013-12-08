847a0900866896b51383079948833959e047048d
byatool <sean@byatool.com>
2013-12-08 11:59:06 -0500

    Partial chain method macro added

--- src/storyboard/macro/clojure_macro.clj
+++ src/storyboard/macro/clojure_macro.clj
@@ -1,49 +1,13 @@
 (ns storyboard.macro.clojure-macro)
 
 
+;; (|->
+;;  (range 0 20)
+;;  (drop 5)
+;;  (take 5)
+;;  (map #(+ 1 %)))
+(defmacro |-> [original & items]
+  `(->
+    ~original
+    ~@(map #(cons (cons 'partial %) '()) items )))
 
-
-;; (defmacro t-t [start & items]
-;;   `(
-;;     ~start
-;;     ~@(map #(cons (cons 'partial %) '()) items )))
-
-
-
-(defmacro t-t [& items]
-  `(
-     ~@(map #(cons (cons 'partial %) '()) items )))
-
-(macroexpand
- '(->
-   [1 2 3]
-   (t-t
-    (apply +)
-    (apply -))))
-(macroexpand
- '(->
-   [1 2 3]
-   ((partial apply +))))
-
-
-
-
-(macroexpand
- '(t-t
-   (apply +)
-   (apply +)
-   (filter #(= 1 %))))
-
-(t-t
- (apply +) 2)
-
-(macroexpand-1
- '(->
-   [1 2 3]
-   ((partial apply +))))
-
-
-
-
-
-(reverse [1 2 3])
